bd1f1f32423ce8ac503d32aaec71d870b6f8206f
Liven dangi <70943451+Liven11@users.noreply.github.com>
2024-01-25 12:02:27 +0530

    Add files via upload

---
+++ src/styles/index.ts
@@ -0,0 +1,44 @@
+export { MuiTheme, ColorPalette } from "./theme";
+export {
+  GlobalStyles,
+  fadeInLeft,
+  fadeIn,
+  slideIn,
+  slideInBottom,
+  StyledLink,
+  pulseAnimation,
+  DialogBtn,
+  PathName,
+} from "./globalStyles";
+export {
+  GreetingHeader,
+  GreetingText,
+  TasksCount,
+  TaskCompletionText,
+  TaskCountHeader,
+  TaskCountTextContainer,
+  TasksCountContainer,
+  ProgressPercentageContainer,
+  Offline,
+  StyledProgress,
+} from "./home.styled";
+export { Container, Input, AddTaskButton, StyledInput } from "./addTask.styled";
+export {
+  TaskComponent,
+  EmojiContainer,
+  TaskDate,
+  TaskInfo,
+  TaskDescription,
+  TaskHeader,
+  NoTasks,
+  CategoriesListContainer,
+  TasksContainer,
+  Pinned,
+  CategoryChip,
+  TimeLeft,
+  TaskName,
+  HighlightedText,
+  SearchInput,
+  RingAlarm,
+  ShowMoreBtn,
+} from "./tasks.styled";
